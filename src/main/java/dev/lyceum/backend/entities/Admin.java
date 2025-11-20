fff1e18ef6ef6e9fb30e29f7dde667fa68a0366a
Andriy <114863893+noinsts@users.noreply.github.com>
2025-11-20 13:20:52 +0200

    feat(entities): add Admin entity

---
+++ src/main/java/dev/lyceum/backend/entities/Admin.java
@@ -0,0 +1,23 @@
+package dev.lyceum.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
